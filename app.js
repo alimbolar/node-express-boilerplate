31d2cf257520e8b797c4883ea53692273dba008c
Alim Bolar <alim@fourplusmedia.com>
2022-02-05 12:26:24 +0800

    experimenting

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const apiRouter = require("./routers/apiRouter");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.get("/", (req, res) => {
 //   //   res.send("Hello World");
@@ -14,6 +15,5 @@ app.set("view engine", "pug");
 
 app.use("/", viewRouter);
 app.use("/api", apiRouter);
-app.use(express.static(path.join(__dirname, "html-boilerplate")));
 
 module.exports = app;
